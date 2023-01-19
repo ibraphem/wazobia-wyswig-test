858c444b17e5f3d8a4d12aeae5876afbba8b78bb
ibraphem <ibraphem@gmail.com>
2023-01-20 00:05:34 +0100

    pushed editor instance to context

--- src/screen/Wyswig.js
+++ src/screen/Wyswig.js
@@ -1,11 +1,12 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { saveEditor } from "../redux/slices/editorSlice";
 
 const Wyswig = () => {
     return (
         <CKEditor
         editor={ClassicEditor}
-        onReady={(editor) => console.log(editor)}
+        onReady={(editor) => dispatch(saveEditor(editor))}
       />
     )
 }
